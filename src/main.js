d89c27eff4b0d1dec4477f7dd6053fd3e3ad3699
bel <126632113+bel-en-s@users.noreply.github.com>
2026-02-25 17:45:01 -0300

    imports

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import "../globals.css";
+
+import "../css/preloader.css";
+import "../css/transition.css";
+import "../css/menu.css";
+import "../css/home.css";
+import "../css/pixelated-video.css";
+import "../css/pixelated-text.css";
+import "../css/spotlight.css";
+import "../css/split-element.css";
+import "../css/footer.css";
+
+import "../scripts/lenis-scroll.js";
+import "../scripts/preloader.js";
+import "../scripts/transition.js";
+import "../scripts/menu.js";
+import "../scripts/hover.js";
+import "../scripts/pixelated-video.js";
+import "../scripts/pixelated-text.js";
+import "../scripts/parallax.js";
+import "../scripts/spotlight.js";
+import "../scripts/split-element.js";
+import "../scripts/clients.js";
