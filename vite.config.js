b49232443d377b79e11d5d6cddc1deb9017549cf
bel <126632113+bel-en-s@users.noreply.github.com>
2026-03-02 19:40:46 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/divino_divino/",
+  base: "./",
   build: {
     rollupOptions: {
       input: {
