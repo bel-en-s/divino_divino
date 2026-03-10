68d1b6f9ec6650152a054e5d45ca4f874f8189a9
Daniela Regert <danielaregert@gmail.com>
2026-03-10 10:15:49 -0300

    debug prod

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,15 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+console.log("Footer script loaded");
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
   trigger: "footer",
   start: "top bottom",
   onEnter: () => {
+    console.log("Entered footer");
     document.querySelector(".logo-container").classList.add("footer-visible");
   },
   onLeaveBack: () => {
+    console.log("Left footer");
     document.querySelector(".logo-container").classList.remove("footer-visible");
   }
 })
