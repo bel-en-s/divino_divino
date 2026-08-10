c7c2b53ee70afce0a4d3c96843eac959ec120868
belen-novaworks <belen.seoane.palmieri@gmail.com>
2026-08-10 12:47:57 -0300

    hotfix build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         culture: resolve(__dirname, "culture.html"),
         directors: resolve(__dirname, "directors.html"),
         contact: resolve(__dirname, "contact.html"),
-        film: resolve(__dirname, "film.html"),
+        draw: resolve(__dirname, "draw.html"),
       },
     },
     assetsInclude: ["**/*.jpeg", "**/*.jpg", "**/*.png", "**/*.svg", "**/*.gif", "**/*.mp4", "**/*.webp", "**/*.hdr"],
